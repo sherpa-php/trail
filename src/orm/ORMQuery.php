c9bece8daa5028f1ff7f713614ef03970b87f093
belicfr <96206878+belicfr@users.noreply.github.com>
2025-01-08 14:22:50 -0500

    feat: add ORMQuery methods

--- src/orm/ORMQuery.php
+++ src/orm/ORMQuery.php
@@ -2,6 +2,7 @@
 
 namespace Sherpa\Trail\orm;
 
+use Sherpa\Db\database\DB;
 use Sherpa\Db\database\Query;
 
 class ORMQuery extends Query
@@ -11,4 +12,31 @@ class ORMQuery extends Query
 
     private array $publicData = [];
     private array $hiddenData = [];
+
+    public function get(array $columns = ["*"]): array
+    {
+        $sql = $this->sql();
+        $parameters = $this->parameters;
+
+        return json_decode(json_encode(DB::run($sql, $parameters)));
+    }
+
+    public function first(array $columns = ["*"]): ?object
+    {
+        return $this->get($columns)[0] ?? null;
+    }
+
+    public function last(array $columns = ["*"]): ?object
+    {
+        $rows = $this->get($columns);
+
+        return array_pop($rows);
+    }
+
+    public function find(mixed $id, array $columns = ["*"], string $idColumn = "id"): ?object
+    {
+        $this->where($idColumn, $id);
+
+        return $this->first($columns);
+    }
 }
