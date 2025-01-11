f64ad1fe0ead2e9aaa756e8e2fa1e28dabe3a072
belicfr <96206878+belicfr@users.noreply.github.com>
2025-01-10 20:11:41 -0500

    feat: create Many-to-Many relationship builder
    #20

--- src/orm/Relationships.php
+++ src/orm/Relationships.php
@@ -29,4 +29,39 @@ trait Relationships
                       ->where($fkName, $fk)
                       ->toRelationshipQuery(Relationship::HAS_MANY);
     }
+
+    public static function makeHasOne(
+        ?int $fk,
+        string $fkName,
+        string $target): ORMRelationshipQuery
+    {
+        return $target::query()
+                      ->where($fkName, $fk)
+                      ->toRelationshipQuery(Relationship::HAS_ONE);
+    }
+
+    public static function makeManyToMany(
+        ?int $leftFk,
+        string $leftFkName,
+        string $rightFkName,
+        string $pivotTable,
+        string $target): ORMRelationshipQuery
+    {
+        $pivot = $pivotTable::query()
+                            ->where($leftFkName, $leftFk)
+                            ->get();
+
+        $query = $target::query();
+
+        foreach ($pivot as $row)
+        {
+            $query->where("id", $row->data->$rightFkName);
+        }
+
+        return $query->toRelationshipQuery(Relationship::MANY_TO_MANY);
+
+//        return $target::query()
+//                      ->where("id", $pivot->data->$rightFkName)
+//                      ->toRelationshipQuery();
+    }
 }
