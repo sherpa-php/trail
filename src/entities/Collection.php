8d1804295f85b75b994dea3dad2ebef403d68342
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-04 03:25:12 -0500

    feat: add `Collection::last` method

--- src/entities/Collection.php
+++ src/entities/Collection.php
@@ -20,4 +20,14 @@ class Collection
             ? $this->models[0]
             : null;
     }
+
+    /**
+     * @return mixed Last collection's model instance
+     */
+    public function last(): mixed
+    {
+        $clone = array_slice($this->models, 0);
+
+        return array_pop($clone);
+    }
 }
